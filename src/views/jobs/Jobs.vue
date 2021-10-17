245332036ac43d8af9ca852fd886a785761098e1
John Connor <zkmd2012@inbox.ru>
2021-10-17 12:35:01 +0500

    3 Jobs page created. 'views ' folder - for pages, 'components' folder - for reusable components

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <h2>{{job.title}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                {title: 'Web designer', id: 1, details: 'lorem'},
+                {title: 'Web developer', id: 2, details: 'lorem2'},
+                {title: 'Painter', id: 3, details: 'lorem3'},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
