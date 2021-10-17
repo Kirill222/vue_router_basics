86daa0dabe242ff56cc925b48ed2b15a8fe636bf
John Connor <zkmd2012@inbox.ru>
2021-10-17 12:19:35 +0500

    2 Another way to use router-link attribute TO

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, deserunt quam magnam repellendus placeat autem eius, vitae officia nobis in sunt modi voluptatum. Quasi velit repudiandae error. Iste, minima fuga?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, deserunt quam magnam repellendus placeat autem eius, vitae officia nobis in sunt modi voluptatum. Quasi velit repudiandae error. Iste, minima fuga?</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto sapiente nulla voluptate consectetur inventore perspiciatis ratione nisi et, facere nesciunt nobis, repellat dignissimos. Odit saepe alias quas laborum eveniet ullam recusandae aperiam voluptas doloribus, magnam consequatur numquam magni modi quos adipisci error corporis nihil amet ea esse iure fuga! Natus quia sed iste, officia hic non impedit! Officia perspiciatis odio aliquam delectus doloribus deserunt illum, ullam inventore dicta earum ducimus expedita quaerat aut porro. Quae optio inventore repellat deleniti vel voluptatem exercitationem unde. Tenetur, ratione. Corrupti, exercitationem tempore culpa ipsum minus magnam, incidunt nesciunt eum, autem fugit velit est.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus iusto sapiente nulla voluptate consectetur inventore perspiciatis ratione nisi et, facere nesciunt nobis, repellat dignissimos. Odit saepe alias quas laborum eveniet ullam recusandae aperiam voluptas doloribus, magnam consequatur numquam magni modi quos adipisci error corporis nihil amet ea esse iure fuga! Natus quia sed iste, officia hic non impedit! Officia perspiciatis odio aliquam delectus doloribus deserunt illum, ullam inventore dicta earum ducimus expedita quaerat aut porro. Quae optio inventore repellat deleniti vel voluptatem exercitationem unde. Tenetur, ratione. Corrupti, exercitationem tempore culpa ipsum minus magnam, incidunt nesciunt eum, autem fugit velit est.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
