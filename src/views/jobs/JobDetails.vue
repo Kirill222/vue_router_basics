15b742300929408403f7ef7ac77580f5196cb120
John Connor <zkmd2012@inbox.ru>
2021-10-17 12:48:49 +0500

    4 Routes with parameters. Id example

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Job details page</h1>
+  <p>The job id is {{job_id}}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            job_id: this.$route.params.job_id,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
